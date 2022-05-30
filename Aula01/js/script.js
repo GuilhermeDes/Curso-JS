bbb9153845a2bb0f20ffbed125d326a13b4ff651
GuilhermeDes <59022920+GuilhermeDes@users.noreply.github.com>
2022-05-30 17:32:32 -0300

    finalizando a aula 6 onde aprendi sobre alerts, variaveis, strings e template.

---
+++ Aula01/js/script.js
@@ -0,0 +1,26 @@
+    //window.alert('Isso aqui é um alerta!')
+
+    //window.confirm('Isso é um alerta com confirmação')
+
+    //window.prompt('Sou um alerta que voce pode digitar! ok?')
+/*
+    var n1 = Number.parseInt(prompt('Digite algum numero: '))
+    var n2 = Number.parseInt(prompt('Digite outro numero: '))
+    var s = n1 + n2
+    window.alert(`O resultado da soma dos dois numeros é: ${s}!`)*/
+
+    //var nome = window.prompt('Digite seu nome: ')
+    //var idade = Number(prompt())
+/*
+    var nome = window.prompt('Qual o seu nome? ')
+    window.alert(`O seu nome é: ${nome}`)
+
+    var nome = window.prompt('Qual o seu nome? ')
+    document.writeln(`O seu nome é ${nome.toUpperCase} e ${nome.toLowerCase} tem ${nome.length} letras!`)
+
+    var salario = window.prompt('Quanto voce ganha por mês?')
+    document.writeln(`O seu salario é de: ${salario.toFixed(2).replace(',', '.')}`)
+
+    var salario = window.prompt('Quanto voce ganha por mês?')
+    document.writeln(`O seu salario é de: ${salario.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})}`)
+    */
